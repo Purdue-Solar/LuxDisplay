bc43634a43d47aa3fcfd2b440c8d26a923071303
Joseph McAlear <36277014+JosephMcAlear@users.noreply.github.com>
2024-06-01 15:28:24 -0400

    Testing without CANSend and Encoder

--- Lux.DriverInterface.Server/Program.cs
+++ Lux.DriverInterface.Server/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddSingleton<Telemetry>();
 builder.Services.AddSingleton<Peripheral>(); //Not needed after the steering wheel gets implemented
 builder.Services.AddSingleton<Encoder>(); // Only necessary for testing
 builder.Services.AddHostedService<TestingDataIncrementService>();
-builder.Services.AddHostedService<CANSendService>();
+//builder.Services.AddHostedService<CANSendService>();
 
 
 //builder.Services.AddHostedService<EncoderService>();
